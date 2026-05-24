b77698a519211573a8b6b465bb93d66ab7915172
conicey <conicey@null.net>
2026-05-25 07:35:08 +0800

    Update request-token.js

--- vault/api/request-token.js
+++ vault/api/request-token.js
@@ -88,7 +88,7 @@ export default async function handler(req, res) {
   `;
 
   const { error: emailError } = await resend.emails.send({
-    from:    'Vault <noreply@yourdomain.com>',
+    from:    'Vault <onboarding@resend.dev>',
     to:      'conicey@null.net',
     subject: 'Vault Access Token',
     html:    emailHtml,
